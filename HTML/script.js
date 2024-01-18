358bc1d41bd51885faa00a2bf6dd4b4270747413
ramiferrot18 <147268248+ramiferrot18@users.noreply.github.com>
2024-01-18 06:04:08 -0400

    Delete HTML directory

--- HTML/script.js
+++
@@ -1,7 +0,0 @@
-function AgregarTarea(){
-    var tarea = document.getElementById("nueva-tarea").value; 
-    var nuevoItem=createElement("li");
-    nuevoItem.textContent=tarea;
-    document.getElementById("tarea-lista").appendChild(nuevoItem);
-    document.getElementById("nueva-tarea").value="";
-}
